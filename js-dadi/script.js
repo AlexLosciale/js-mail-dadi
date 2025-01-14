cf0de2c835da8086f30469992addfda6f2f9e329
AlexLosciale <losciale17@gmail.com>
2025-01-14 12:48:57 +0100

    ADD:start

---
+++ js-dadi/script.js
@@ -0,0 +1,15 @@
+/**
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+*/
+
+/*
+1-CREARE UN NUMERO RANDOMICO DA 1 A 6 
+  1.2-CREARLO PER 2 PERSONE (PC/PERSONA)
+2-INSERIRE IL RILSTATO IN UN IF, COSI DA STABILIRE IL VINCITORE DEL GIOCO
+3-STAMPARE IL RISULTATO 
+*/
---
+++ js-mail/script.js
@@ -0,0 +1,18 @@
+/*
+Mail
+Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+Si può fare? Certo che si basta ragionare un po’.
+Nota:
+Non è necessario provvedere alla validazione delle email
+*/
+
+/**
+ 1-CREARE UN ARREY CON ALCUNE EMAIL
+ 2-CREARE UN PROMPT DOVE FARE INSERIRE EMAIL ALL'UTENTE
+ 3-CONTROLLARE CON UN CICLO SE EMAIL INSERITA E PRESENTE NELLA LISTA ARREY
+ 4-STAMPARE IL RISULTATO IN BASE ALL'ESITO DEL CONTROLLO 
+ */
