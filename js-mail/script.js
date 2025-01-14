4a41828b2c33877c79c0302aa458a5f77c04796c
AlexLosciale <losciale17@gmail.com>
2025-01-14 14:49:38 +0100

    add:email

--- js-mail/script.js
+++ js-mail/script.js
@@ -15,4 +15,30 @@ Non è necessario provvedere alla validazione delle email
  2-CREARE UN PROMPT DOVE FARE INSERIRE EMAIL ALL'UTENTE
  3-CONTROLLARE CON UN CICLO SE EMAIL INSERITA E PRESENTE NELLA LISTA ARREY
  4-STAMPARE IL RISULTATO IN BASE ALL'ESITO DEL CONTROLLO 
- */
+*/
+
+const email = [
+    'pippo@gmail.com',
+    'topolino@gmail.com',
+    'paperino@gmail.com',
+    'pluto@gmail.com',
+];
+
+console.log(email);
+
+let emailPippo = prompt("Inserisci la tua email:");
+
+let found = false;
+
+for (let index = 0; index < email.length; index++) {
+    if (emailPippo === email[index]) {
+        found = true;
+        break;
+    }
+}
+
+if (found) {
+    console.log("SEI STATO INVITATO ALLA FESTA");
+} else {
+    console.log("NON SEI NELLA LISTE DEGLI INVITATI");
+}
